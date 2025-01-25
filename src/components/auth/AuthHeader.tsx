5668981cdc21509147b4c912e1b3127e4dccfe8d
Ardian Eka Candra <ardianoption@gmail.com>
2025-01-26 01:38:12 +0700

    fix typing size

--- src/components/auth/AuthHeader.tsx
+++ src/components/auth/AuthHeader.tsx
@@ -9,14 +9,14 @@ export const AuthHeader = ({ description }: Props) => {
     <>
       <header className="flex items-center justify-center gap-2">
         <img src={logo} alt="Logo" className="size-10" />
-        <span className="text-lg font-medium">
+        <span className="text-xl font-semibold">
           SIMS PPOB <span className="hidden md:inline">ARDIAN </span>
           <span>EKA</span>
           <span className="hidden md:inline"> CANDRA</span>
         </span>
       </header>
 
-      <p className="mb-12 mt-6 text-center text-2xl font-medium">
+      <p className="mb-12 mt-6 text-center text-3xl font-medium">
         {description}
       </p>
     </>
